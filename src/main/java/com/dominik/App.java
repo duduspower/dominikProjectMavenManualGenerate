2bd21e68d947396a79097884375949250eedcfa3
Dominik Duda <dominikduda191@gmail.com>
2022-01-05 09:53:43 +0100

    First commit

---
+++ src/main/java/com/dominik/App.java
@@ -0,0 +1,13 @@
+package com.dominik;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "No siemka!" );
+    }
+}
